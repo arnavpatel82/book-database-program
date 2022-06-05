0f71bceab0b66453a690a124b3177e12020e620e
arnavpatel82 <arnav.patel82@yahoo.com>
2022-06-05 22:20:47 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,6 @@
+# create a database
+# books.db
+
+# create a model
+#title, author, date_publish, price
+
---
+++ models.py
@@ -0,0 +1,10 @@
+# import models
+
+# main menu - add, search, analysis, exit, view
+# add books to db
+# edit books
+# delete books
+# search books
+# data cleaning functions
+
+# loop runs program
